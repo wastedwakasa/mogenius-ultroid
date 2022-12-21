f9a3024674f793f0a955b41f0a4ab8aee90a1966
wastedwakasa <103997368+wastedwakasa@users.noreply.github.com>
2022-12-21 18:59:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,4 +9,4 @@ def hello_world():
 
 # ex https://Leoksu:ghp_147bkkabcdefgh@github.com/Leoksu/Ultroid
 
-os.system("git clone https://wastedwakasa:@gith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ithub.com/wastedwakasa/Ultroids ok && cd ok && pip3 install -r requirements.txt && pip3 install -r re*/st*/op* && python3 -m pyUltroid &")
+os.system("git clone https://wastedwakasa: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@github.com/wastedwakasa/Ultroids ok && cd ok && pip3 install -r requirements.txt && pip3 install -r re*/st*/op* && python3 -m pyUltroid &")
